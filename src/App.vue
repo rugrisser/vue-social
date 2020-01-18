d5c8a46f6a99e21f98f68adbc3107230ecf62198
Вячеслав Рыжков <rugrisser@gmail.com>
2020-01-18 14:19:24 +0300

    root template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app id="app">
+        <v-navigation-drawer color="light-blue darken-1" dark expand-on-hover hide-overlay permanent right app>
+
+        </v-navigation-drawer>
+        <v-content class="px-12 py-3">
+            <v-container fluid>
+                <router-view/>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
