7b87b64f3446f5e16e68fac444152ad3a6abb182
Вячеслав Рыжков <rugrisser@gmail.com>
2020-01-19 00:28:51 +0300

    Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="home">
-
+        <v-container class="text-left mainBlock">
+            <p class="light-blue--text text--darken-2 display-2 font-weight-bold">SocialLink</p><br>
+            <p class="light-blue--text text--darken-3 display-1 font-weight-bold">Новый формат делового общения</p>
+            <v-row>
+                <v-col cols="5">
+                    <img width="100%" src="https://images.pexels.com/photos/3183186/pexels-photo-3183186.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+                </v-col>
+                <v-col cols="6">
+                    <p class="subtitle-1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a elit eu nisl semper porta. Nulla eget vestibulum urna. Pellentesque turpis urna, eleifend consectetur nulla vitae, tempus lobortis velit. Morbi molestie tincidunt tincidunt. Aliquam ut libero at sem placerat lobortis. Aliquam ut felis id dui interdum bibendum sed nec mi. Nullam non sapien ut orci varius gravida. Aliquam erat volutpat. Praesent efficitur auctor diam, a euismod ipsum facilisis id. Quisque et posuere orci. Sed ac congue dui, vel porta mauris. Curabitur eget magna eget nulla luctus viverra. Nulla a accumsan odio. Aenean vel neque sit amet nulla rutrum maximus.
+                    </p>
+                    <p class="subtitle-1">
+                        Duis viverra est et lorem consequat fringilla. Sed ut varius quam. Sed bibendum consectetur purus, at pulvinar ligula ultrices id. Pellentesque a tellus quis risus venenatis cursus. Donec scelerisque magna non auctor consequat. Nunc sit amet leo a mauris aliquam egestas sagittis eu elit. Quisque in tellus urna.
+                    </p>
+                    <p class="subtitle-1">
+                        Donec iaculis eleifend nisl at auctor. Fusce eu porta diam. Quisque vestibulum lectus vitae turpis dapibus laoreet sed vel ex. Nam velit felis, gravida vel tellus id, vehicula vulputate lacus. Maecenas gravida arcu nec ipsum eleifend, sed commodo mauris blandit. Nam odio ex, varius et felis faucibus, iaculis feugiat arcu. Nullam tempus eleifend risus, et varius nulla aliquet in. Quisque posuere felis et tortor imperdiet aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in mauris nibh. Quisque pharetra velit quis iaculis feugiat. Pellentesque eget quam turpis. Pellentesque egestas mattis felis.
+                    </p>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
@@ -11,3 +30,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+    .mainBlock {
+        margin-top: 3% !important;
+        margin-left: 3% !important;
+    }
+
+</style>
