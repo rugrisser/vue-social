c19185f44d9ff09d173c2244d10055acea8da31b
Вячеслав Рыжков <rugrisser@gmail.com>
2020-02-28 00:26:46 +0300

    Posts

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -2,25 +2,47 @@
     <div class="posts">
         <v-container class="text-left mainBlock">
             <p class="light-blue--text text--darken-2 display-2 font-weight-bold">Последние публикации</p><br>
-            <div v-if="id > 0">
-                <v-text-field
-                        solo label="Заголовок"
-                        style="width: 40%;" />
-                <v-textarea
-                        solo style="width: 40%;"
-                        label="Что у вас нового?" />
-                <v-btn class="px-9" x-large color="primary">Опубликовать</v-btn>
-            </div>
             <v-container class="mt-8">
                 <v-row>
-                    <v-col
-                            cols="10"
-                            v-for="(post, index) in posts"
-                            :key="index">
+                        <v-col cols="10" sm="10" md="5">
+                        <v-card v-if="id > 0" outlined class="mb-6">
+                            <v-container class="px-12 py-12">
+                                <div>
+                                    <v-text-field
+                                            v-model="newPost.title"
+                                            outlined label="Заголовок"
+                                            style="width: 100%;" />
+                                    <v-textarea
+                                            v-model="newPost.text"
+                                            outlined style="width: 100%;"
+                                            label="Что у вас нового?" />
+                                    <v-btn
+                                            class="px-9" x-large
+                                            color="primary" @click="createPost"
+                                            style="text-align: center;">Опубликовать</v-btn>
+                                </div>
+                            </v-container>
+                        </v-card>
+                        <div
+                                :key="index"
+                                v-for="(post, index) in posts.first">
+                            <Post
+                                    class="mb-6"
+                                    :avatar="post.user.photo"
+                                    :author="post.user.name"
+                                    :text="post.text"
+                                    :title="post.title"/>
+                        </div>
+                    </v-col>
+                    <v-col cols="10" sm="10" md="5">
                         <Post
-                                :author="profile.name"
-                                :title="post.title"
-                                :text="post.body" />
+                                :key="index"
+                                v-for="(post, index) in posts.second"
+                                class="mb-6"
+                                :avatar="post.user.photo"
+                                :author="post.user.name"
+                                :text="post.text"
+                                :title="post.title"/>
                     </v-col>
                 </v-row>
             </v-container>
@@ -38,27 +60,97 @@
         },
         data() {
             return {
-                posts: [],
+                posts: {
+                    data: [],
+                    main: [],
+                    first: [],
+                    second: []
+                },
+                newPost: {
+                    title: "",
+                    text: ""
+                },
                 users: [],
                 id: -1
+            }
+        },
+        methods: {
+            getPosts() {
+
+                this.axios.get('http://188.225.47.187/api/jsonstorage/3aa45f3959e052317fba82f4096df045')
+                    .then(
+                        (response) => {
+
+                            this.posts.main = response.data;
+                            this.posts.data = response.data;
+                            this.posts.main.reverse();
+
+                        }
+                    );
+                this.axios.get('http://188.225.47.187/api/jsonstorage/4e5b70b015290d296c13945601023e8d')
+                    .then(
+                        (response) => {
+
+                            this.users = response.data;
+
+                            for (const index in this.posts.main) {
+
+                                let author_id = this.posts.main[index].author;
+                                let author = this.users[author_id - 1];
+                                this.posts.main[index].user = author;
+
+                            }
+
+                            for (const index in this.posts.main) {
+
+                                switch (index % 2) {
+                                    case 0:
+                                        if (this.id === -1)
+                                            this.posts.first.push(this.posts.main[index]);
+                                        else
+                                            this.posts.second.push(this.posts.main[index]);
+                                        break;
+                                    case 1:
+                                        if (this.id === -1)
+                                            this.posts.second.push(this.posts.main[index]);
+                                        else
+                                            this.posts.first.push(this.posts.main[index]);
+                                        break;
+                                }
+
+                            }
+
+                        }
+                    );
+
+            },
+            createPost() {
+
+                if (this.id !== -1) {
+
+                    const post = {
+                        title: this.newPost.title,
+                        text: this.newPost.text,
+                        author: this.id
+                    };
+
+                    this.posts.data.push(post);
+
+                    this.axios.put('http://188.225.47.187/api/jsonstorage/3aa45f3959e052317fba82f4096df045', JSON.stringify(this.posts.data))
+                        .then(
+                            () => {
+                                this.getPosts();
+                            }
+                        );
+
+                }
+
             }
         },
         mounted() {
 
             this.id = this.$store.state.id;
-
-            this.axios.get('http://188.225.47.187/api/jsonstorage/3aa45f3959e052317fba82f4096df045')
-                .then(
-                    (response) => {
-                        this.posts = response.data;
-                    }
-                );
-            this.axios.get('http://188.225.47.187/api/jsonstorage/4e5b70b015290d296c13945601023e8d')
-                .then(
-                    (response) => {
-                        this.users = response.data;
-                    }
-                );
+            this.getPosts();
 
         }
     }
