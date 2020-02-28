3f11c9faf8568d97311c2afa68de9dc0c311faae
Вячеслав Рыжков <rugrisser@gmail.com>
2020-02-28 22:37:49 +0300

    Profile Posts

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,14 +3,14 @@
         <span class="light-blue--text text--darken-2 display-2 font-weight-bold"><v-icon color="light-blue darken-2" x-large>mdi-account-outline</v-icon> {{ profile.name }}</span>
         <v-container class="py-10">
             <v-row>
-                <v-col cols="3">
+                <v-col lg="2" md="3" sm="4">
                     <v-avatar
                         width="200px"
                         height="200px">
                         <img :src="profile.photo">
                     </v-avatar>
                 </v-col>
-                <v-col cols="9" class="body-1">
+                <v-col cols="8" class="body-1">
                     <br><span>Веб-сайт: <a :href="'https://' + profile.website" target="_blank">{{ profile.website }}</a></span><br><br>
                     <span>E-mail: <a :href="'mailto:' + profile.email">{{ profile.email }}</a> </span><br><br>
                     <span>Город: {{ profile.city }}</span><br><br>
@@ -18,19 +18,29 @@
                 </v-col>
             </v-row>
         </v-container>
-        <v-divider class="py-7"></v-divider>
+        <v-divider class="py-7 mr-12"></v-divider>
         <span class="display-1 font-weight-bold">Публикации</span>
         <v-container class="mt-8">
             <v-row>
-                <v-col
-                        cols="10"
-                        v-for="(post, index) in posts"
-                        :key="index">
+                <v-col xl="5" lg="5" md="5" sm="10" col="10">
                     <Post
-                        :avatar="'https://randomuser.me/api/portraits/men/' + parseInt($route.params.id) + '.jpg'"
-                        :author="profile.name"
-                        :title="post.title"
-                        :text="post.body" />
+                            v-for="(post, index) in posts.first"
+                            class="mb-6"
+                            :key="index"
+                            :title="post.title"
+                            :text="post.text"
+                            :author="profile.name"
+                            :avatar="profile.photo" />
+                </v-col>
+                <v-col xl="5" lg="5" md="5" sm="10" col="10">
+                    <Post
+                            v-for="(post, index) in posts.second"
+                            class="mb-6"
+                            :key="index"
+                            :title="post.title"
+                            :text="post.text"
+                            :author="profile.name"
+                            :avatar="profile.photo" />
                 </v-col>
             </v-row>
         </v-container>
@@ -56,7 +66,12 @@
                     company: "Apple",
                     photo: "https://vk.com/images/camera_200.png"
                 },
-                posts: []
+                posts: {
+                    data: [],
+                    main: [],
+                    first: [],
+                    second: []
+                }
             }
 
         },
@@ -79,7 +94,31 @@
                                     this.profile = response.data[id - 1];
 
                             }
-                        )
+                        );
+
+                    this.axios.get('http://188.225.47.187/api/jsonstorage/3aa45f3959e052317fba82f4096df045')
+                        .then(
+                            (response) => {
+
+                                this.posts.data = response.data;
+                                this.posts.main = this.posts.data.filter(post => post.author === id);
+                                this.posts.main.reverse();
+
+                                for (const index in this.posts.main) {
+
+                                    switch (index % 2) {
+                                        case 0:
+                                            this.posts.first.push(this.posts.main[index]);
+                                            break;
+                                        case 1:
+                                            this.posts.second.push(this.posts.main[index]);
+                                            break;
+                                    }
+
+                                }
+
+                            }
+                        );
 
                 },
                 immediate: true
